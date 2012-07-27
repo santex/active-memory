c0ef259103390d5eb1ff7423bc3f33994bea0f87
santex <santex@cpan.org>
2012-07-27 16:38:27 +0200

    concept mining with spock & santex

--- onliners.sh
+++ onliners.sh
@@ -10,23 +10,27 @@ perl -Micro -E "print say+microname;"
 
 
 #SHOW 10 RANDOM NODES
-micro any 10; echo "(returns 10 random node)";
+micro any 10; 
+echo "(returns 10 random node)";
 #OR
 perl -Micro -E "print say+microname+10;"
 
 
 
 #SHOW 8 RANDOM PLANETS
-micro planets 8; echo " (returns 8 random nodes from micro structure planets)";
+micro planets 8; 
+echo " (returns 8 random nodes from micro structure planets)";
 
 
 #CREATE CHAOS 
-sudo micro new chaos; echo " (creates micro structure chaos)";
+sudo micro new chaos; 
+echo " (creates micro structure chaos)";
 
 
 
 #DUMP CHAOS 
-sudo micro drop chaos; echo " (deletes micro structure chaos)";
+sudo micro drop chaos; 
+echo " (deletes micro structure chaos)";
 
 #DUMP TOP NODES
 perl -MAI::MicroStructure -le 'print for AI::MicroStructure->themes;'
