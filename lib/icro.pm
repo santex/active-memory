9e929036ee630e23f873c7e18dd2ca4e85162bed
santex <santex@grml.gchq.ml>
2015-01-27 02:37:59 +0100

    removing legacy

--- lib/icro.pm
+++
@@ -1,8 +0,0 @@
-package icro;
-use strict;
-use AI::MicroStructure;
-our @ISA = qw(AI::MicroStructure);
-our $VERSION = '1.000';
-"holy cow";
-
-__END__
--- scripts/test003.pl
+++
@@ -1,9 +0,0 @@
-use Term::ReadKey;
-ReadMode('cbreak');
-print "Press keys to see their ASCII values.  Use Ctrl-C to quit.\n";
-while (1) {
-    $char = ReadKey(0);
-    last unless defined $char;
-    printf(" Decimal: %d\tHex: %x\n", ord($char), ord($char));
-}
-ReadMode('normal'); 
